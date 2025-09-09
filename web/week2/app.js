cca3790f5890195c458e067ae9f1b7ca514bc511
Shyun Moon <shyunmoon@college.harvard.edu>
2025-09-09 16:43:59 -0400

    Q5

--- web/week2/app.js
+++ web/week2/app.js
@@ -5,9 +5,12 @@ console.log("Hello World");
 let answer = prompt('What is 1+1?');
 console.log(answer);
 
-const radData = "Programming for Psychologists";
-console.log(radData.toLowerCase());
-console.log(radData.toUpperCase());
+console.log('Programming for Psychologists'.toUpperCase());
 
+const tagline = "Programming for Psychologists";
+console.log(tagline.indexOf("Psychologists")); // 16
 
+let browserType = "Programming for Psychologists";
+browserType = browserType.replace("Programming", "Coding");
+console.log(browserType);
 
