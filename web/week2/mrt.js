e27ceecf221ba0071b0132a3a9253be8c3a6ff46
Shyun Moon <shyunmoon@college.harvard.edu>
2025-09-09 16:47:39 -0400

    Q5

--- web/week2/mrt.js
+++ web/week2/mrt.js
@@ -1,12 +1,12 @@
-prompt("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.")
+alert("In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.")
 
 let answer1 = prompt('What is 1+1?');
-prompt('You answered: ' + answer1);
+alert('You answered: ' + answer1);
 
 let answer2 = prompt('What is 1+2?');
-prompt('You answered: ' + answer2);
+alert('You answered: ' + answer2);
 
 let answer3 = prompt('What is 1+3?');
-prompt('You answered: ' + answer3);
+alert('You answered: ' + answer3);
 
-prompt("Thank you for your participation!")
+alert("Thank you for your participation!")
