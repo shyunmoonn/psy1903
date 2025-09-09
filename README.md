caf0d9c22142f428c8ca02c443db23213d243efc
Shyun Moon <shyunmoon@college.harvard.edu>
2025-09-09 16:07:18 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # psy1903
+## Shyun Moon
 Course work for psy1903
---
+++ web/week2/app.js
@@ -0,0 +1,13 @@
+console.log('Hello World!');
+console.log('Hello again...');
+console.log("Hello World");
+
+let answer = prompt('What is 1+1?');
+console.log(answer);
+
+const radData = "Programming for Psychologists";
+console.log(radData.toLowerCase());
+console.log(radData.toUpperCase());
+
+
+
