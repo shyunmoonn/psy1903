c7049784d8764ee3f2af50af043cef04b688e14f
Shyun Moon <shyunmoon@college.harvard.edu>
2025-09-11 18:43:26 -0400

    Q9

--- web/week2/mrt.js
+++ web/week2/mrt.js
@@ -4,7 +4,7 @@ let start1 = Date.now()
 let answer1 = prompt('What is ' + (Math.floor(Math.random() * 10) + 1) + ' + ' + (Math.floor(Math.random() * 10) + 1));
 let end1 = Date.now()
 let result1 = (end1 - start1) / 1000
-alert('You answered ' + answer1 + ' in ' + result1 + " seconds");
+alert('You answered ' + answer1 + ' in ' + result1 + 'seconds');
 
 let start2 = Date.now()
 let answer2 = prompt('What is ' + (Math.floor(Math.random() * 10) + 1) + ' + ' + (Math.floor(Math.random() * 10) + 1));
