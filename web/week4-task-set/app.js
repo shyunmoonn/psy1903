9794c73a5c5e893ee4e83acad7637826e9094baa
Shyun Moon <shyunmoon@college.harvard.edu>
2025-09-23 16:33:10 -0400

    Q3

--- web/week4-task-set/app.js
+++ web/week4-task-set/app.js
@@ -1,8 +1,24 @@
+/*
 let celsius = [];
 function celsiusToFahrenheit(celsius) {
     let fahrenheit = (celsius * 1.8) + 32
     return fahrenheit
 }
 console.log(celsiusToFahrenheit(10));
+*/
+
+
+function convertTemp(temp, convertTo) {
+    if (
+        convertTo == 'c') {
+        return (temp - 32) / 1.8
+    } else {
+        return (temp * 1.8) + 32
+    }
+}
+console.log(convertTemp(10, 'f')); // Expected output: 50
+
+
+
 
 
